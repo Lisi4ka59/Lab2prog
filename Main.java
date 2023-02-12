f818c5d6eec15a0609e516ec45bfe19d309879b6
Lisi4ka59 <0059nai@gmail.com>
2023-02-12 16:19:17 +0300

    new

--- Main.java
+++ Main.java
@@ -10,12 +10,12 @@ public class Main {
 
     public static void main(String[] args) {
         Battle battle = new Battle();
-        battle.addAlly(new Whiscash("Whiscash", 3));
-        battle.addAlly(new Girafarig("Girafarig", 4));
-        battle.addAlly(new Roselia("Roselia", 2));
-        battle.addFoe(new Barboach("Barboach", 5));
-        battle.addFoe(new Budew("Budew", 1));
-        battle.addFoe(new Roserade("Roserade", 3));
+        battle.addAlly(new Whiscash("Союзник 1", 3));
+        battle.addAlly(new Girafarig("Союзник 2", 4));
+        battle.addAlly(new Roselia("Союзник 3", 2));
+        battle.addFoe(new Barboach("Враг 1", 5));
+        battle.addFoe(new Budew("Враг 2", 1));
+        battle.addFoe(new Roserade("Враг 3", 3));
         battle.go();
     }
 }
